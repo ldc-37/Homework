0b178cf1e7a5e493a282b593826b41676a9958c2
ldc-37 <ldc791551236@gmail.com>
2018-03-31 12:00:53 +0800

    initial

---
+++ List/list.h
@@ -0,0 +1,18 @@
+#pragma once
+
+void ShowCopyright ();
+void ShowTime ();
+//Head* Iniciate ();
+char GetInputUI ();
+//Head* CreateChainRandom ();
+void PrintNode (int);
+//void SaveToFile (Head*);
+void ShowChain ();
+void ShowIllegalNode ();
+void DeleteNode ();
+void Incert ();
+void ShowDays ();
+void Sort ();
+void PrimeNumberNode ();
+void ReverseChain ();
+void FreeChain ();
