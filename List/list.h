577d3311ef93da695fafcb5a03ac32c7a370e229
ldc-37 <ldc791551236@gmail.com>
2018-04-05 15:58:35 +0800

    create&output

--- List/list.h
+++ List/list.h
@@ -5,24 +5,48 @@
 #include <cstdlib>
 #include <conio.h>
 
+#define NODE_NUM 2000
+#define MIN_YEAR 1900
+#define MAX_YEAR 2300
+#define MONTH_NUM 12
+#define DAY_NUM 31
+
+#ifndef _NODE_DEFINED
+typedef struct Date
+{
+    int year;
+    int month;
+    int day;
+    Date *next;
+}date;
+#define _NODE_DEFINED
+#endif // !_NODE_DEFINED
+
+#ifndef _DAYOFMONTH_DEFINED
+const int dayOfMonth[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+#define _DAYOFMONTH_DEFINED
+#endif // !_DAYOFMONTH_DEFINED
+
+
 #ifndef list_h__
 #define list_h__
 
 void ShowCopyright ();
 void ShowTime ();
-//Head* Iniciate ();
+Date * InitList ();
 void UserInterface ();
 char GetOption ();
-//Head* CreateChainRandom ();
-void PrintNode (int);
+Date * CreateListRandom (Date*);
+void PrintList (Date*);
 void SaveToFile ();
-void ShowChain ();
+void ShowListFile ();
 void ShowIllegalNode ();
 void DeleteNode ();
 void Incert ();
 void ShowDays ();
 void Sort ();
 void PrimeNumberNode ();
-void ReverseChain ();
-void FreeChain ();
+void ReverseList ();
+void FreeList ();
 #endif // list_h__
+
