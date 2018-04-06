65bca8869d45f7a30c094986a6382c3ad4a5523b
ldc-37 <ldc791551236@gmail.com>
2018-04-06 20:08:11 +0800

    finishing

--- List/list.h
+++ List/list.h
@@ -1,15 +1,15 @@
-#pragma once
-
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
-#include <conio.h>
+#include <ctime> //use to get time
+#include <conio.h> // use to get key-event
+#include <cmath> // use to judge prime number
 
-#define NODE_NUM 2000
+#define INIT_NODE_NUM 2000
 #define MIN_YEAR 1900
 #define MAX_YEAR 2300
 #define MONTH_NUM 12
 #define DAY_NUM 31
+
 
 #ifndef _NODE_DEFINED
 typedef struct Date
@@ -31,22 +31,38 @@ const int dayOfMonth[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 #ifndef list_h__
 #define list_h__
 
+//Main
 void ShowCopyright ();
 void ShowTime ();
 Date * InitList ();
 void UserInterface ();
-char GetOption ();
 Date * CreateListRandom (Date*);
 void PrintList (Date*);
 void SaveToFile ();
 void ShowListFile ();
-void ShowIllegalNode ();
-void DeleteNode ();
+
+bool IsReapYear (int);
+bool IsIllegalDate (Date*);
+void ShowIllegalNode (Date*);
+void DeleteNode (Date*);
+
 void Incert ();
-void ShowDays ();
+
+void ShowTotalDays (Date*);
+int CountDays (Date*);
+
 void Sort ();
-void PrimeNumberNode ();
-void ReverseList ();
-void FreeList ();
+
+bool IsPrimeNumber (int);
+void PrimeNumberNode (Date*);
+
+void ReverseList (Date*);
+void FreeList (Date*);
+
+//Assist
+bool GetOptionYN ();
+char GetOption (Date*);
+bool IsEmptyLink (Date*);
+int NodeNum (Date*);
 #endif // list_h__
 
